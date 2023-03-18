c8edd7c070cb753b26affb65782dc7982847fef4
marley265 <jackndiritu97@gmail.com>
2023-03-18 15:12:48 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,7 +8,7 @@
  * FUNCTIONALITY *
  *
  * 1. Declare a variable called letter of type int.
- * 2. Start a for loop that will run as long as letter is greater than or equal to the ASCII value of ‘a’.
+ * 2. For loop  letter is greater than or equal to the ASCII value of ‘a’.
  * 3. Inside the for loop, print the value of letter.
  * 4. Decrement the value of letter by 1.
  * 5. End the for loop.
