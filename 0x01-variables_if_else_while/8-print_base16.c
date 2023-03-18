4be17b3860ada7b5eae061271ab2caa6f7f6753a
marley265 <jackndiritu97@gmail.com>
2023-03-18 15:19:58 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Print all single digits of base 16 followed by new line
+ *
+ * Return: returns 0
+ *
+ * FUNCTONALITY *
+ *
+ * 1. We declare a variable called digit and initialize it to 0.
+ * 2. Start a for loop that runs as long as digit is less than or equal to 9.
+ * 3. We print the value of digit.
+ * 4. We increment digit by 1.
+ * 5. If digit is equal to 9, we set it to a.
+ * 6. Start a for loop that runs as long as digit is less than or equal to f.
+ * 7. We print the value of digit.
+ * 8. We increment digit by 1.
+ * 9. We print a new line.
+ * 10. We return 0.
+ *
+ * Time Complexity: O(1)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
