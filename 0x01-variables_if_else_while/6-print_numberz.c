69af9decd5cf260e37037a9379e4761f61da58e2
marley265 <jackndiritu97@gmail.com>
2023-03-18 15:03:05 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints all single digits of base 10 followed by new line
+ *
+ * Return: return 0
+ *
+ * FUNCTIONALITY *
+ *
+ * 1. Declare a variable called digit of type int.
+ * 2. Initialize the variable digit to the character ‘0’.
+ * 4. Print a newline character.
+ * 5. Return 0.
+ *
+ * Time Complexity: O(1)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
