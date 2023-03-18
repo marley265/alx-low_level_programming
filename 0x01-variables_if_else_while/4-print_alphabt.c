3f432b627f21fd7f8e46641b24a9a498dc832b2e
marley265 <jackndiritu97@gmail.com>
2023-03-18 14:28:31 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Program to alphabet letters ecxept q and e followed by new line
+ *
+ * Return: return 0
+ *
+ * FUNCTIONALITY *
+ *
+ * 1. For loop is initialized  letter = 'a' and the condition is letter <= 'z'.
+ *
+ * 2. The loop will continue until the condition is false.
+ *
+ * 3. The if statement checks if letter is equal to 'q' or 'e'.
+ *
+ * 4. If the condition is true, the continue statement is executed and the loop
+ * continues with the next iteration.
+ *
+ * 5. If the cond is false, the putchar func is executed, letter is printed.
+ *
+ * 6. The loop continues until the condition is false.
+ *
+ * 7. The putchar function prints a new line.
+ *
+ * 8. The return statement returns 0.
+ *
+ * Time Complexity: O(n)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
