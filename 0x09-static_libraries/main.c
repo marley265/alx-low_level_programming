ed4493ad4eed8602fc9b872ee9dcc7897c952270
marley265 <jackndiritu97@gmail.com>
2023-04-08 23:45:37 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Main - Entry point
+ * -puts: prints a string
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	-puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
